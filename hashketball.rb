ecbee21e054072c209b2ec73fcf84b5a4c21f940
chaim <chaimyisroel@gmail.com>
2021-09-19 22:19:28 -0500

    commit tree 4d7d4af205c40ad68a0198a5a8da6bb90e1d96ae

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require('pry')
 def game_hash
   {
     home: {
@@ -127,3 +128,36 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored name
+  game_hash.each do |team, value|
+    value.each do |key, value|
+      # puts key
+      
+      if key == :players
+        value.each do |key, value|
+          if key[:player_name] == name
+            return key[:points]
+          end
+        end
+      end
+    end
+  end
+end
+
+p num_points_scored "Jeff Adrien"
+
+def shoe_size name
+  game_hash.each do |team, value|
+    value.each do |key, value|
+      # puts key
+      
+      if key == :players
+        value.each do |key, value|
+          if key[:player_name] == name
+            return key[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
